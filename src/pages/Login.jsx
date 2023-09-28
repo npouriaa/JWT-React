963b498a9d45758e78c772af6f13f70f1c0ab896
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-09-28 12:29:23 +0330

    style login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Button, Form, Input } from "antd";
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Form
+      className="form"
+      labelCol={{
+        span: 8,
+      }}
+    >
+      <h1>Login</h1>
+      <Form.Item
+        label="Username"
+        name="username"
+        className="form-item"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-export default Login
+      <Form.Item
+        label="Password"
+        name="password"
+        className="form-item"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item className="form-item button-item">
+        <Button className="button" type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
