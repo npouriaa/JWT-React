524676490153d156ba381cdfc2f802cb5689aa84
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-09-28 13:25:21 +0330

    handle signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,39 @@
 import { Button, Form, Input } from "antd";
-import React from "react";
+import React, { useState } from "react";
+import authService from "../services/auth.service";
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [successful, setSuccessful] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    setMessage("");
+    setSuccessful(false);
+
+    authService.register(username, email, password).then((response) => {
+      setMessage(response.data.message);
+      setSuccessful(true);
+    }),
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setSuccessful(false);
+        setMessage(resMessage);
+      };
+  };
+
   return (
     <Form
+      onFinish={(e) => handleSignup(e)}
       className="form"
       labelCol={{
         span: 8,
@@ -21,7 +51,7 @@ const SignUp = () => {
           },
         ]}
       >
-        <Input />
+        <Input onChange={(e) => setUsername(e.target.value)} />
       </Form.Item>
       <Form.Item
         label="Email"
@@ -38,7 +68,7 @@ const SignUp = () => {
           },
         ]}
       >
-        <Input />
+        <Input onChange={(e) => setEmail(e.target.value)} />
       </Form.Item>
 
       <Form.Item
@@ -52,7 +82,7 @@ const SignUp = () => {
           },
         ]}
       >
-        <Input.Password />
+        <Input.Password onChange={(e) => setPassword(e.target.value)} />
       </Form.Item>
 
       <Form.Item className="form-item button-item">
