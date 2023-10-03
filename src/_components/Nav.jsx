d1421da4552653852fbee755bdd1f52806ec28b8
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-10-03 19:30:02 +0330

    code Nav component

--- src/_components/Nav.jsx
+++ src/_components/Nav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../redux/auth-slice";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const authUser = useSelector((x) => x.auth.user);
+  const dispatch = useDispatch();
+  const logout = () => dispatch(authActions.logout());
+  // only show nav when logged in
+  if (!authUser) return null;
 
-export default Nav
+  return (
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <div className="navbar-nav">
+        <NavLink to="/" className="nav-item nav-link">
+          Home
+        </NavLink>
+        <button onClick={logout} className="btn btn-link nav-item nav-link">
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
